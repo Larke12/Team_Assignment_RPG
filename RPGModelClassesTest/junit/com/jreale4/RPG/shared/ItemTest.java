93d78e5e52e3b2d727d40faa350357e979885f71
John Reale IV <jreale@ycp.edu>
2014-03-19 11:30:04 -0400

    Edited ItemTest

--- RPGModelClassesTest/junit/com/jreale4/RPG/shared/ItemTest.java
+++ RPGModelClassesTest/junit/com/jreale4/RPG/shared/ItemTest.java
@@ -1,7 +1,15 @@
 package com.jreale4.RPG.shared;
 
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 public class ItemTest extends TestCase{
-
+	private Item item1;
+	private Item item2;
+	private Item item3;
+	private Item item4;
+	private Item item5;
+	ArrayList<Item> i = new ArrayList<Item>();
+	
 }
