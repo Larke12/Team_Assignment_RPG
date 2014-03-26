780b6f907dec985bb6cdcec07fee57799313b6c3
John Reale <jreale@ycp.edu>
2014-03-26 11:24:31 -0400

    Everything is reset....i hope

---
+++ RPGModelClassesTest/junit/com/jreale4/RPG/shared/AttackTest.java
@@ -0,0 +1,50 @@
+package com.jreale4.RPG.shared;
+
+import java.util.ArrayList;
+import junit.framework.TestCase;
+
+import com.jreale4.RPG.shared.Attack;
+import com.jreale4.RPG.shared.Move;
+import com.jreale4.RPG.shared.MoveType;
+
+
+public class AttackTest extends TestCase {
+
+	private Attack attack1;
+	private Attack attack2;
+	private Attack attack3;
+	private Attack attack4;
+	ArrayList<Attack> a = new ArrayList<Attack>();
+
+	protected void setUp(){
+		attack1= new Attack(MoveType.flamenPunch, Move.physical);
+		attack2= new Attack(MoveType.aqua, Move.magic);
+		attack3= new Attack(MoveType.gigaFlamenBreak, Move.physical);
+		attack4= new Attack(MoveType.thundara, Move.magic);
+		a.add(attack1);
+		a.add(attack2);
+		a.add(attack3);
+		a.add(attack4);
+	}
+
+	public void testGetMoveType(){
+		assertEquals(MoveType.flamenPunch,attack1.getMoveType());
+		assertEquals(MoveType.aqua,attack2.getMoveType());
+		assertEquals(MoveType.gigaFlamenBreak,attack3.getMoveType());
+		assertEquals(MoveType.thundara,attack4.getMoveType());
+	}
+
+	public void testGetMove(){
+		assertEquals(Move.physical,attack1.getMove());
+		assertEquals(Move.magic,attack2.getMove());
+		assertEquals(Move.physical,attack3.getMove());
+		assertEquals(Move.magic,attack4.getMove());
+	}
+
+	public void testGetPower(){
+		assertEquals( (float)(1.5), attack1.getAttackPower(attack1.getMoveType(), attack1.getMove(), 1));
+		assertEquals( (float)(3), attack2.getAttackPower(attack2.getMoveType(), attack2.getMove(), 1));
+		assertEquals( (float)(1), attack3.getAttackPower(attack3.getMoveType(), attack3.getMove(), 1));
+		assertEquals( (float)(3.5), attack4.getAttackPower(attack4.getMoveType(), attack4.getMove(), 1));
+	}
+}
