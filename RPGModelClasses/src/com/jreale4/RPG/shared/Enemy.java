e0d220b931bb65d15aa95acffc56e22c42a42b4c
John Reale <jreale@ycp.edu>
2014-03-28 11:14:41 -0400

    Altered Enemy class

--- RPGModelClasses/src/com/jreale4/RPG/shared/Enemy.java
+++ RPGModelClasses/src/com/jreale4/RPG/shared/Enemy.java
@@ -4,14 +4,36 @@ import java.util.ArrayList;
 
 public class Enemy extends Character {
 	
-	public Enemy(int Level, ArrayList<Attack> attacklist, ArrayList<Item> itemlist, ArrayList<Equipment> equiplist){
-		this.Level=Level;
-		this.AttackList=attacklist;
-		this.ItemList=itemlist;
-		this.EquipList=equiplist;
+	public Enemy(int HeroLvl){
+		this.AttackList=  CreateAttacks();
+		this.EquipList= CreateEquip();
+		this.ItemList= CreateItems();
+		this.Level= CreateLevel(HeroLvl);
 	}
 	
+	private int CreateLevel(int level){
+		float Lvl=level*(450/350);
+		
+		return (int)Lvl;
+	}
+	private ArrayList<Attack> CreateAttacks(){
+		ArrayList<Attack> atklst= new ArrayList<Attack>();
+		
+		return atklst;
+	}
+	private ArrayList<Item> CreateItems(){
+		ArrayList<Item> itemlst= new ArrayList<Item>();
+		
+		return itemlst;
+	}
+	private ArrayList<Equipment> CreateEquip(){
+		ArrayList<Equipment> equiplst= new ArrayList<Equipment>();
+		
+		return equiplst;
+	}
 	// Randomize monster, selected monster uses a random moveset focused
 	// around that type of move set (physical, all normal attacks. magic, 
 	// magical moves based on their attribute
+	
+	
 }
