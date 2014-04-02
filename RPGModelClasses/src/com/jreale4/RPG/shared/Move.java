63805abd0ae13c665ec76a7a7a44f267704b42fa
Ryan Hammett <larke12@linkshideaway.com>
2014-04-02 11:39:04 -0400

    Updated move class to extend coverage (removed null return value).

--- RPGModelClasses/src/com/jreale4/RPG/shared/Move.java
+++ RPGModelClasses/src/com/jreale4/RPG/shared/Move.java
@@ -2,14 +2,12 @@ package com.jreale4.RPG.shared;
 
 public enum Move {
 	physical, magic;
-	
+
 	public static Move locationOf(int i){
 		if(i==1){
 			return physical;
 		}
-		if(i==2){
-			return magic;
-		}
-		return null;
+		// Else it is magic.
+		return magic;
 	}
 }
