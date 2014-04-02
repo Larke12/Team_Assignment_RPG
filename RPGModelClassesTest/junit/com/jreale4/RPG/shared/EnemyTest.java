5bc19be04710ec5ea0316cf15f7310dd942daf6a
Ryan Hammett <larke12@linkshideaway.com>
2014-04-02 11:32:22 -0400

    Updated EnemyTest class.

--- RPGModelClassesTest/junit/com/jreale4/RPG/shared/EnemyTest.java
+++ RPGModelClassesTest/junit/com/jreale4/RPG/shared/EnemyTest.java
@@ -8,8 +8,6 @@ public class EnemyTest extends TestCase{
 
 	private Enemy enemy1;
 
-	private int lvl;
-
 	private Attack attack1;
 	private Attack attack2;
 	private Attack attack3;
@@ -20,44 +18,6 @@ public class EnemyTest extends TestCase{
 	private Item attUp;
 	private Item acid;
 	ArrayList<Item> i = new ArrayList<Item>();
-
-	private Equipment helmet;
-	private Equipment torso;
-	private Equipment bracers;
-	private Equipment boots;
-	ArrayList<Equipment> e = new ArrayList<Equipment>();
-
-	protected void setUp(){
-
-		lvl=1;
-
-		attack1= new Attack(MoveType.slash, Move.physical);
-		attack2= new Attack(MoveType.aqua, Move.magic);
-		attack3= new Attack(MoveType.venta, Move.magic);
-		attack4= new Attack(MoveType.flamenKick, Move.physical);
-		a.add(attack1);
-		a.add(attack2);
-		a.add(attack3);
-		a.add(attack4);
-
-		potion= new Item(ItemType.health);
-		attUp= new Item(ItemType.boost);
-		acid= new Item(ItemType.poison);
-		i.add(potion);
-		i.add(attUp);
-		i.add(acid);
-
-		helmet= new Equipment(EquipmentType.head, 1);
-		torso= new Equipment(EquipmentType.torso, 2);
-		bracers= new Equipment(EquipmentType.arms, 1);
-		boots= new Equipment(EquipmentType.feet, 3);
-		e.add(helmet);
-		e.add(torso);
-		e.add(bracers);
-		e.add(boots);
-
-		enemy1= new Enemy(lvl);
-	}
 
 	public void testGetMoveType(){
 		assertEquals(MoveType.slash,attack1.getMoveType());
@@ -82,26 +42,5 @@ public class EnemyTest extends TestCase{
 		assertEquals(5, enemy1.ItemList.get(enemy1.ItemList.indexOf(potion)).getItemEffect(enemy1.Level));
 		assertEquals(3, enemy1.ItemList.get(enemy1.ItemList.indexOf(attUp)).getItemEffect(enemy1.Level));
 		assertEquals(-5, enemy1.ItemList.get(enemy1.ItemList.indexOf(acid)).getItemEffect(enemy1.Level));	
-	}
-
-	public void testEquipLvl(){
-		assertEquals(1, enemy1.EquipList.get(enemy1.EquipList.indexOf(helmet)).getEquipLvl());
-		assertEquals(2, enemy1.EquipList.get(enemy1.EquipList.indexOf(torso)).getEquipLvl());
-		assertEquals(1, enemy1.EquipList.get(enemy1.EquipList.indexOf(bracers)).getEquipLvl());
-		assertEquals(3, enemy1.EquipList.get(enemy1.EquipList.indexOf(boots)).getEquipLvl());
-	}
-
-	public void testEquipType(){
-		assertEquals(EquipmentType.head, enemy1.EquipList.get(enemy1.EquipList.indexOf(helmet)).getEquipmentType());
-		assertEquals(EquipmentType.torso, enemy1.EquipList.get(enemy1.EquipList.indexOf(torso)).getEquipmentType());
-		assertEquals(EquipmentType.arms, enemy1.EquipList.get(enemy1.EquipList.indexOf(bracers)).getEquipmentType());
-		assertEquals(EquipmentType.feet, enemy1.EquipList.get(enemy1.EquipList.indexOf(boots)).getEquipmentType());
-	}
-
-	public void testEquipBonus(){
-		assertEquals(1, enemy1.EquipList.get(enemy1.EquipList.indexOf(helmet)).getEquipBonus());
-		assertEquals(3, enemy1.EquipList.get(enemy1.EquipList.indexOf(torso)).getEquipBonus());
-		assertEquals(1, enemy1.EquipList.get(enemy1.EquipList.indexOf(bracers)).getEquipBonus());
-		assertEquals(5, enemy1.EquipList.get(enemy1.EquipList.indexOf(boots)).getEquipBonus());
 	}
 }
