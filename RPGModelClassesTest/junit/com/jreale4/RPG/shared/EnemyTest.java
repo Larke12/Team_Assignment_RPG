8c34d380d01704c1380c9ed12c585f3c65bd66ca
John Reale <jreale@ycp.edu>
2014-04-02 11:34:32 -0400

    Thing

---
+++ RPGModelClassesTest/junit/com/jreale4/RPG/shared/EnemyTest.java
@@ -0,0 +1,26 @@
+package com.jreale4.RPG.shared;
+
+import junit.framework.TestCase;
+
+public class EnemyTest extends TestCase{
+	
+	private Enemy enemy1;
+	
+	
+	public void testGetMoveType(){
+		
+	}
+	
+	public void testGetMove(){
+		
+	}
+	
+	public void testGetPower(){
+		
+	}
+	
+	public void testItemEffect(){
+		
+	}
+	
+}
