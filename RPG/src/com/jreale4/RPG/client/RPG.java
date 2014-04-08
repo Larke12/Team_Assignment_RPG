de8f9e0b1d065f580cbba492ab9c13aead31d32c
John Reale <jreale@ycp.edu>
2014-04-08 15:02:58 -0400

    Added Starting Login View

--- RPG/src/com/jreale4/RPG/client/RPG.java
+++ RPG/src/com/jreale4/RPG/client/RPG.java
@@ -1,6 +1,8 @@
 package com.jreale4.RPG.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -10,5 +12,10 @@ public class RPG implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		LoginView view = new LoginView();
+		
+		RootLayoutPanel.get().add(view);
+		RootLayoutPanel.get().setWidgetLeftRight(view, 0.0, Unit.PX, 0.0, Unit.PX);
+		RootLayoutPanel.get().setWidgetTopBottom(view, 0.0, Unit.PX, 0.0, Unit.PX);
 	}
 }
