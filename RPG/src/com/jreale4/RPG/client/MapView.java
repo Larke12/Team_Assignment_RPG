6ef10095eee9411062080dd9685138dc067832c5
Ryan Hammett <larke12@linkshideaway.com>
2014-04-14 13:45:37 -0400

    Updated MapView, includes zoom of map (fix later).
    Invisibile button on MapView to be tested.

--- RPG/src/com/jreale4/RPG/client/MapView.java
+++ RPG/src/com/jreale4/RPG/client/MapView.java
@@ -4,6 +4,7 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.Button;
 
 public class MapView extends Composite {
 
@@ -12,10 +13,19 @@ public class MapView extends Composite {
 		LayoutPanel layoutPanel = new LayoutPanel();
 		initWidget(layoutPanel);
 		
-		Image image = new Image((String) null);
+		Image image = new Image("rpg/gwt/clean/images/rpg_mapped.png");
 		layoutPanel.add(image);
-		layoutPanel.setWidgetLeftWidth(image, 0.0, Unit.PX, 100.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(image, 0.0, Unit.PX, 100.0, Unit.PX);
+		// Does nothing
+		image.setSize("1920", "1080");
+		
+		layoutPanel.setWidgetLeftWidth(image, 0.0, Unit.PX, 1920.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(image, 0.0, Unit.PX, 1080.0, Unit.PX);
+		
+		Button btnNewButton = new Button("New button");
+		btnNewButton.setVisible(false);
+		layoutPanel.add(btnNewButton);
+		layoutPanel.setWidgetLeftWidth(btnNewButton, 166.0, Unit.PX, 81.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(btnNewButton, 118.0, Unit.PX, 28.0, Unit.PX);
 		
 	}
 }
