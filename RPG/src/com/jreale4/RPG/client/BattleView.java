ef28b727bcfc336d3e98818cca76cf9683ce47f2
Ryan Hammett <larke12@linkshideaway.com>
2014-04-22 18:09:04 -0400

    If and only if login is successful, receives Stack Overflow error.
    Updates to BattleView layout and button code.

--- RPG/src/com/jreale4/RPG/client/BattleView.java
+++ RPG/src/com/jreale4/RPG/client/BattleView.java
@@ -1,14 +1,20 @@
 package com.jreale4.RPG.client;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.DoubleClickHandler;
+import com.google.gwt.event.dom.client.DoubleClickEvent;
 
 public class BattleView extends Composite {
+	
+	//IsWidget iView = new InView();
+	IsWidget mView = new MapView();
 
 	public BattleView() {
 		
@@ -27,24 +33,53 @@ public class BattleView extends Composite {
 		layoutPanel.setWidgetTopHeight(btnAttack, 365.0, Unit.PX, 28.0, Unit.PX);
 		
 		Button btnInventory = new Button("Inventory");
+		btnInventory.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				// Open Inventory View
+				//RPG.setView(InView);
+			}
+		});
 		layoutPanel.add(btnInventory);
 		layoutPanel.setWidgetLeftWidth(btnInventory, 600.0, Unit.PX, 81.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(btnInventory, 365.0, Unit.PX, 28.0, Unit.PX);
 		
 		Button btnEscape = new Button("Escape!");
+		btnEscape.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				// Random counter, if < 1 does nothing; if > 1 goes back to map
+				RPG.setView(mView);
+			}
+		});
 		layoutPanel.add(btnEscape);
 		layoutPanel.setWidgetLeftWidth(btnEscape, 600.0, Unit.PX, 81.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(btnEscape, 415.0, Unit.PX, 28.0, Unit.PX);
 		
 		Button btnMagica = new Button("Magica");
+		btnMagica.addDoubleClickHandler(new DoubleClickHandler() {
+			public void onDoubleClick(DoubleClickEvent event) {
+				// Use last used magic attack
+			}
+		});
+		btnMagica.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				// Open selection of images, current view
+			}
+		});
 		layoutPanel.add(btnMagica);
 		layoutPanel.setWidgetLeftWidth(btnMagica, 490.0, Unit.PX, 81.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(btnMagica, 415.0, Unit.PX, 28.0, Unit.PX);
 		
 		Image image = new Image("rpg/gwt/clean/images/character_skew.png");
 		layoutPanel.add(image);
-		layoutPanel.setWidgetLeftWidth(image, 0.0, Unit.PX, 100.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(image, 0.0, Unit.PX, 100.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(image, 0.0, Unit.PX, 317.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(image, 0.0, Unit.PX, 519.0, Unit.PX);
+		
+		Image image_1 = new Image("rpg/gwt/clean/images/enemy_xx.png");
+		image_1.setAltText("Random Enemy Image!");
+		layoutPanel.add(image_1);
+		image_1.setSize("1111px", "335px");
+		layoutPanel.setWidgetLeftWidth(image_1, 318.0, Unit.PX, 742.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(image_1, 47.0, Unit.PX, 281.0, Unit.PX);
 		
 	}
 }
