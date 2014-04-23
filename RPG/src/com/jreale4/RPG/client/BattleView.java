fe916931c8467ae18012dc877cd0b0d6021019be
John Reale <jreale@ycp.edu>
2014-04-23 11:19:48 -0400

    Escape no longer works every time

--- RPG/src/com/jreale4/RPG/client/BattleView.java
+++ RPG/src/com/jreale4/RPG/client/BattleView.java
@@ -1,5 +1,7 @@
 package com.jreale4.RPG.client;
 
+import java.util.Random;
+
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.LayoutPanel;
@@ -10,16 +12,24 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.DoubleClickHandler;
 import com.google.gwt.event.dom.client.DoubleClickEvent;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class BattleView extends Composite {
 	
 	//IsWidget iView = new InView();
-
+	TextBox textBox;
+	
 	public BattleView() {
 		
 		LayoutPanel layoutPanel = new LayoutPanel();
 		initWidget(layoutPanel);
 		layoutPanel.setSize("800px", "800px");
+
+		textBox = new TextBox();
+		textBox.setVisible(false);
+		layoutPanel.add(textBox);
+		layoutPanel.setWidgetLeftWidth(textBox, 490.0, Unit.PX, 173.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(textBox, 496.0, Unit.PX, 34.0, Unit.PX);
 		
 		Button btnAttack = new Button("Attack!");
 		btnAttack.addClickHandler(new ClickHandler() {
@@ -45,8 +55,15 @@ public class BattleView extends Composite {
 		Button btnEscape = new Button("Escape!");
 		btnEscape.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				// Random counter, if < 1 does nothing; if > 1 goes back to map
+				Random rand = new Random();
+				int cnt = rand.nextInt(6)-2;
+				if(cnt > 0){
 				RPG.setView(new MapView());
+				}
+				else{
+					textBox.setText("Can't Escape!");
+					textBox.setVisible(true);
+				}
 			}
 		});
 		layoutPanel.add(btnEscape);
@@ -68,17 +85,12 @@ public class BattleView extends Composite {
 		layoutPanel.setWidgetLeftWidth(btnMagica, 490.0, Unit.PX, 81.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(btnMagica, 415.0, Unit.PX, 28.0, Unit.PX);
 		
-		Image image = new Image("rpg/gwt/clean/images/character_skew.png");
-		layoutPanel.add(image);
-		layoutPanel.setWidgetLeftWidth(image, 0.0, Unit.PX, 317.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(image, 0.0, Unit.PX, 519.0, Unit.PX);
-		
 		Image image_1 = new Image("rpg/gwt/clean/images/enemy_xx.png");
 		image_1.setAltText("Random Enemy Image!");
 		layoutPanel.add(image_1);
 		image_1.setSize("1111px", "335px");
-		layoutPanel.setWidgetLeftWidth(image_1, 318.0, Unit.PX, 761.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(image_1, 47.0, Unit.PX, 320.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(image_1, 328.0, Unit.PX, 888.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(image_1, 16.0, Unit.PX, 310.0, Unit.PX);
 		
 	}
 }
